2293d1464a679768d85861ba5c8434f76ffe8b03
Mike Lehmann <mlehmann@gmail.com>
2021-11-10 11:12:02 -0500

    Changed with my data to run manually

--- Recent-Tweet-Counts/recent_tweet_counts.js
+++ Recent-Tweet-Counts/recent_tweet_counts.js
@@ -1,50 +1,48 @@
 // Search for Tweets within the past seven days
 // https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
 
-const needle = require('needle');
+const needle = require("needle");
 
 // The code below sets the bearer token from your environment variables
 // To set environment variables on macOS or Linux, run the export command below from the terminal:
 // export BEARER_TOKEN='YOUR-TOKEN'
-const token = process.env.BEARER_TOKEN;
+const token =
+  "AAAAAAAAAAAAAAAAAAAAAHDWVQEAAAAATSf%2Fo8y%2BIk5BmYQDMqHh6Mfgsns%3DgnxqULA7YAL427mE9cPXZIsNwo17F0V7TY5k08fXQbyLkUPyiE";
 
 const endpointUrl = "https://api.twitter.com/2/tweets/counts/recent";
 
 async function getRequest() {
+  // Edit query parameters below and specify a search query
+  // optional params: start_time,end_time,since_id,until_id,next_token,granularity
+  const params = {
+    query: `"safemoon"`,
+    granularity: "minute",
+  };
 
-    // Edit query parameters below and specify a search query
-    // optional params: start_time,end_time,since_id,until_id,next_token,granularity
-    const params = {
-        'query': 'from:twitterdev',
-        'granularity': 'day'
-    }
+  const res = await needle("get", endpointUrl, params, {
+    headers: {
+      "User-Agent": "v2RecentTweetCountsJS",
+      authorization: `Bearer ${token}`,
+    },
+  });
 
-    const res = await needle('get', endpointUrl, params, {
-        headers: {
-            "User-Agent": "v2RecentTweetCountsJS",
-            "authorization": `Bearer ${token}`
-        }
-    })
-
-    if (res.body) {
-        return res.body;
-    } else {
-        throw new Error('Unsuccessful request');
-    }
+  if (res.body) {
+    return res.body;
+  } else {
+    throw new Error("Unsuccessful request");
+  }
 }
 
 (async () => {
-
-    try {
-        // Make request
-        const response = await getRequest();
-        console.dir(response, {
-            depth: null
-        });
-
-    } catch (e) {
-        console.log(e);
-        process.exit(-1);
-    }
-    process.exit();
+  try {
+    // Make request
+    const response = await getRequest();
+    console.dir(response, {
+      depth: null,
+    });
+  } catch (e) {
+    console.log(e);
+    process.exit(-1);
+  }
+  process.exit();
 })();
